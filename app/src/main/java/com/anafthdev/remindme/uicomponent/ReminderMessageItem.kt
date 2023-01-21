91c9d7cee1c90d54114244804c3a7ef6c0be7166
kafri8889 <kafri8889@gmail.com>
2023-01-22 00:18:43 +0700

    new component `ReminderMessageItem` for `ReminderMessageType.Add`

--- app/src/main/java/com/anafthdev/remindme/uicomponent/ReminderMessageItem.kt
+++ app/src/main/java/com/anafthdev/remindme/uicomponent/ReminderMessageItem.kt
@@ -1,10 +1,17 @@
 package com.anafthdev.remindme.uicomponent
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -56,6 +63,67 @@ fun ReminderMessageItem(
 					painter = painterResource(id = R.drawable.ic_trash),
 					contentDescription = null
 				)
+			}
+		}
+	}
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun ReminderMessageItem(
+	modifier: Modifier = Modifier,
+	onSave: (String) -> Unit = {}
+) {
+	
+	val focusRequester = remember { FocusRequester() }
+	
+	var message by remember { mutableStateOf("") }
+	
+	LaunchedEffect(Unit) {
+		focusRequester.requestFocus()
+	}
+
+	OutlinedCard(
+		modifier = modifier
+	) {
+		Row(
+			verticalAlignment = Alignment.CenterVertically,
+			modifier = Modifier
+				.padding(8.dp)
+		) {
+			BasicTextField(
+				value = message,
+				singleLine = true,
+				onValueChange = { s ->
+					message = s
+				},
+				modifier = Modifier
+					.weight(1f)
+					.focusRequester(focusRequester)
+			)
+			
+			Spacer(modifier = Modifier.width(8.dp))
+			
+			IconButton(
+				enabled = message.isNotBlank(),
+				onClick = {
+					onSave(message)
+				}
+			) {
+				AnimatedVisibility(
+					visible = message.isNotBlank(),
+					enter = scaleIn(
+						animationSpec = tween(250)
+					),
+					exit = scaleOut(
+						animationSpec = tween(250)
+					)
+				) {
+					Icon(
+						imageVector = Icons.Rounded.Check,
+						contentDescription = null
+					)
+				}
 			}
 		}
 	}
