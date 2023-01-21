a4a74ce9945dc6c4335dc0955fa4789080df3ce8
kafri8889 <kafri8889@gmail.com>
2023-01-21 21:34:48 +0700

    add spacer

--- app/src/main/java/com/anafthdev/remindme/ui/main/MainScreen.kt
+++ app/src/main/java/com/anafthdev/remindme/ui/main/MainScreen.kt
@@ -1,10 +1,7 @@
 package com.anafthdev.remindme.ui.main
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
@@ -206,6 +203,10 @@ fun RemindMeReminderDetail(
 		
 		item {
 			ReminderDetailScreen()
+		}
+		
+		item {
+			Spacer(modifier = Modifier.height(24.dp))
 		}
 	}
 }
