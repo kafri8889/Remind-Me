881f64f5a14857904d3f97c6752973d0dfb24e2a
kafri8889 <kafri8889@gmail.com>
2023-01-22 10:36:36 +0700

    new extension

---
+++ app/src/main/java/com/anafthdev/remindme/extension/ReminderExt.kt
@@ -0,0 +1,16 @@
+package com.anafthdev.remindme.extension
+
+import com.anafthdev.remindme.data.local.model.ReminderDb
+import com.anafthdev.remindme.data.model.Reminder
+
+fun Reminder.toReminderDb(): ReminderDb {
+	return ReminderDb(
+		id = id,
+		name = name,
+		hour = hour,
+		minute = minute,
+		messages = messages,
+		repeatOnDays = repeatOnDays,
+		isActive = isActive
+	)
+}
