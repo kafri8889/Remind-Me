946e06d4604b1b5bdbc66d52bcd61e6767eac294
kafri8889 <kafri8889@gmail.com>
2023-01-22 23:26:05 +0700

    Add setting icon

--- app/src/main/java/com/anafthdev/remindme/ui/setting/SettingScreen.kt
+++ app/src/main/java/com/anafthdev/remindme/ui/setting/SettingScreen.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.anafthdev.remindme.R
@@ -45,6 +47,12 @@ fun SettingScreen(
 				},
 				summary = {
 					Text(stringResource(id = R.string.setting_use_24_hour_format_summary))
+				},
+				icon = {
+					Icon(
+						painter = painterResource(id = R.drawable.ic_clock),
+						contentDescription = null
+					)
 				}
 			)
 			
@@ -58,6 +66,12 @@ fun SettingScreen(
 				},
 				summary = {
 					Text(stringResource(id = R.string.setting_use_auto_save_summary))
+				},
+				icon = {
+					Icon(
+						painter = painterResource(id = R.drawable.ic_receive_square),
+						contentDescription = null
+					)
 				}
 			)
 		}
