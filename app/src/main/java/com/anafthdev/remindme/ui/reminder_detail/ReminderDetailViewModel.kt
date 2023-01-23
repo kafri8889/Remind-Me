60b8da518f7d5d3c8b43f491b295293e22dd66f8
kafri8889 <kafri8889@gmail.com>
2023-01-23 14:50:52 +0700

    Implement auto save

--- app/src/main/java/com/anafthdev/remindme/ui/reminder_detail/ReminderDetailViewModel.kt
+++ app/src/main/java/com/anafthdev/remindme/ui/reminder_detail/ReminderDetailViewModel.kt
@@ -5,20 +5,30 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.anafthdev.remindme.data.DayOfWeek
 import com.anafthdev.remindme.data.HourClockType
 import com.anafthdev.remindme.data.ReminderMessageType
 import com.anafthdev.remindme.data.TimeType
 import com.anafthdev.remindme.data.model.Reminder
+import com.anafthdev.remindme.data.repository.ReminderRepository
+import com.anafthdev.remindme.data.repository.UserPreferencesRepository
 import com.anafthdev.remindme.extension.convert12HourTo24Hour
 import com.anafthdev.remindme.extension.convert24HourTo12Hour
+import com.anafthdev.remindme.extension.toReminderDb
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ReminderDetailViewModel @Inject constructor(
-
+	private val userPreferencesRepository: UserPreferencesRepository,
+	private val reminderRepository: ReminderRepository
 ): ViewModel() {
+	
+	var reminderName by mutableStateOf("")
+		private set
 	
 	var hours by mutableStateOf(0)
 		private set
@@ -26,19 +36,28 @@ class ReminderDetailViewModel @Inject constructor(
 	var minutes by mutableStateOf(0)
 		private set
 	
-	var selectedTimeType by mutableStateOf(TimeType.Hours)
-		private set
-	
 	var clockPositionValue by mutableStateOf(0)
-	    private set
+		private set
 	
 	var animateClockPositionValue by mutableStateOf(false)
 	    private set
 	
+	var isReminderActive by mutableStateOf(false)
+		private set
+	
+	var is24Hour by mutableStateOf(false)
+		private set
+	
+	var autoSave by mutableStateOf(false)
+		private set
+	
+	var selectedTimeType by mutableStateOf(TimeType.Hours)
+		private set
+	
 	var hourClockType by mutableStateOf(HourClockType.AM)
 		private set
 	
-	var reminderName by mutableStateOf("")
+	var currentReminder by mutableStateOf(Reminder.Null)
 		private set
 	
 	var messages = mutableStateListOf<Pair<String, ReminderMessageType>>()
@@ -47,16 +66,30 @@ class ReminderDetailViewModel @Inject constructor(
 	var repeatOnDays = mutableStateListOf<DayOfWeek>()
 		private set
 	
+	init {
+		viewModelScope.launch {
+			userPreferencesRepository.getUserPreferences.collect { preferences ->
+				is24Hour = preferences.is24Hour
+				autoSave = preferences.autoSave
+			}
+		}
+	}
+	
 	fun updateWithReminder(reminder: Reminder) {
-		updateReminderName(reminder.name)
+		updateReminderName(reminder.name, false)
+		updateIsReminderActive(reminder.isActive, false)
 		
+		hours = reminder.hour
+		minutes = reminder.minute
+		currentReminder = reminder
+		clockPositionValue = if (selectedTimeType == TimeType.Hours) hours + 1 else minutes + 1
 		messages.apply {
 			clear()
 			addAll(reminder.messages.map { it to ReminderMessageType.Fixed })
 		}
 	}
 	
-	fun updateClockPosition(pos: Int) {
+	fun updateClockPosition(pos: Int, save: Boolean = true) {
 		clockPositionValue = pos
 		
 		when (selectedTimeType) {
@@ -70,6 +103,8 @@ class ReminderDetailViewModel @Inject constructor(
 				minutes = (pos - 1).coerceAtLeast(0)
 			}
 		}
+		
+		if (autoSave && save) saveReminder()
 	}
 	
 	fun updateTimeType(type: TimeType) {
@@ -83,7 +118,7 @@ class ReminderDetailViewModel @Inject constructor(
 		animateClockPositionValue = true
 	}
 	
-	fun updateHourClockType(type: HourClockType) {
+	fun updateHourClockType(type: HourClockType, save: Boolean = true) {
 		hourClockType = type
 		
 		hours = when (type) {
@@ -92,21 +127,48 @@ class ReminderDetailViewModel @Inject constructor(
 		}
 		
 		clockPositionValue = convert24HourTo12Hour(hours).first.toInt()
+		
+		if (autoSave && save) saveReminder()
 	}
 	
 	fun updateAnimateClockPositionValue(animate: Boolean) {
 		animateClockPositionValue = animate
 	}
 	
-	fun updateRepeatOnDays(days: List<DayOfWeek>) {
+	fun updateRepeatOnDays(days: List<DayOfWeek>, save: Boolean = true) {
 		repeatOnDays.apply {
 			clear()
 			addAll(days)
 		}
+		
+		if (autoSave && save) saveReminder()
 	}
 	
-	fun updateReminderName(name: String) {
+	fun updateReminderName(name: String, save: Boolean = true) {
 		reminderName = name
+		
+		if (autoSave && save) saveReminder()
+	}
+	
+	fun updateIsReminderActive(active: Boolean, save: Boolean = true) {
+		isReminderActive = active
+		
+		if (autoSave && save) saveReminder()
+	}
+	
+	fun saveReminder() {
+		viewModelScope.launch(Dispatchers.IO) {
+			reminderRepository.updateReminder(
+				currentReminder.copy(
+					name = reminderName,
+					hour = hours,
+					minute = minutes,
+					messages = messages.map { it.first },
+					repeatOnDays = repeatOnDays,
+					isActive = isReminderActive
+				).toReminderDb()
+			)
+		}
 	}
 	
 }
