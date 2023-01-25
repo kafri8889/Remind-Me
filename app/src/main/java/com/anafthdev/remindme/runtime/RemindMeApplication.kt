06346e2958fa21f2a2d45835e2d0fdeffe71de99
kafri8889 <kafri8889@gmail.com>
2023-01-25 22:45:29 +0700

    create notification channel

--- app/src/main/java/com/anafthdev/remindme/runtime/RemindMeApplication.kt
+++ app/src/main/java/com/anafthdev/remindme/runtime/RemindMeApplication.kt
@@ -1,15 +1,23 @@
 package com.anafthdev.remindme.runtime
 
 import android.app.Application
+import android.os.Build
 import com.anafthdev.remindme.BuildConfig
+import com.anafthdev.remindme.common.RemindMeNotificationManager
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
 class RemindMeApplication: Application() {
 	
+	@Inject lateinit var remindMeNotificationManager: RemindMeNotificationManager
 	override fun onCreate() {
 		super.onCreate()
 		if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+		
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			remindMeNotificationManager.createChannel()
+		}
 	}
 }
