597ebe6e840afef757882d33743b27ba685a9b7b
kafri8889 <kafri8889@gmail.com>
2023-01-27 21:03:52 +0700

    update next trigger time

--- app/src/main/java/com/anafthdev/remindme/receiver/ReminderReceiver.kt
+++ app/src/main/java/com/anafthdev/remindme/receiver/ReminderReceiver.kt
@@ -5,23 +5,25 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import com.anafthdev.remindme.common.RemindMeAlarmManager
 import com.anafthdev.remindme.common.RemindMeNotificationManager
 import com.anafthdev.remindme.data.AlarmAction
 import com.anafthdev.remindme.data.model.Reminder
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Calendar
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class ReminderReceiver: BroadcastReceiver() {
 	
 	@Inject lateinit var remindMeNotificationManager: RemindMeNotificationManager
+	@Inject lateinit var remindMeAlarmManager: RemindMeAlarmManager
 	
 	override fun onReceive(context: Context, intent: Intent?) {
 		if (intent?.action == null) return
 		
 		when (intent.action) {
 			AlarmAction.ACTION_NOTIFY -> {
-				// TODO: update nextTrigger
 				val bundle = intent.getBundleExtra("bundle") ?: Bundle.EMPTY
 				val reminder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
 					bundle.getParcelable("reminder", Reminder::class.java)
@@ -29,6 +31,15 @@ class ReminderReceiver: BroadcastReceiver() {
 					bundle.getParcelable("reminder") as? Reminder
 				} ?: Reminder.Null
 				
+				val nextTrigger =  Calendar.getInstance().apply {
+					add(Calendar.DAY_OF_MONTH, 1)
+					
+					set(Calendar.HOUR_OF_DAY, reminder.hour)
+					set(Calendar.MINUTE, reminder.minute)
+					set(Calendar.SECOND, 0)
+				}.timeInMillis
+				
+				remindMeAlarmManager.startReminder(reminder, nextTrigger)
 				remindMeNotificationManager.notifyReminder(reminder)
 			}
 		}
