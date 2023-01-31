2add9b545010f69d7b212ec4457239d0d42b4e15
kafri8889 <kafri8889@gmail.com>
2023-01-31 08:46:04 +0700

    add source code setting preference

--- app/src/main/java/com/anafthdev/remindme/ui/setting/SettingScreen.kt
+++ app/src/main/java/com/anafthdev/remindme/ui/setting/SettingScreen.kt
@@ -1,5 +1,7 @@
 package com.anafthdev.remindme.ui.setting
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -9,11 +11,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.anafthdev.remindme.R
 import com.anafthdev.remindme.data.RemindMeRoute
+import com.anafthdev.remindme.uicomponent.BasicPreference
 import com.anafthdev.remindme.uicomponent.RemindMeTopAppBar
 import com.anafthdev.remindme.uicomponent.SwitchPreference
 import com.anafthdev.remindme.utils.RemindMeContentType
@@ -24,6 +28,8 @@ fun SettingScreen(
 	contentType: RemindMeContentType,
 	onBackPressed: () -> Unit = {}
 ) {
+	
+	val context = LocalContext.current
 	
 	LazyColumn(
 		modifier = Modifier
@@ -71,6 +77,28 @@ fun SettingScreen(
 					Icon(
 						painter = painterResource(id = R.drawable.ic_receive_square),
 						contentDescription = null
+					)
+				}
+			)
+			
+			Spacer(modifier = Modifier.height(8.dp))
+			
+			BasicPreference(
+				title = {
+					Text(stringResource(id = R.string.source_code))
+				},
+				icon = {
+					Icon(
+						painter = painterResource(id = R.drawable.ic_github_mark),
+						contentDescription = null
+					)
+				},
+				onClick = {
+					context.startActivity(
+						Intent(Intent.ACTION_VIEW).apply {
+							flags = Intent.FLAG_ACTIVITY_NEW_TASK
+							data = Uri.parse("https://github.com/kafri8889/Remind-Me")
+						}
 					)
 				}
 			)
